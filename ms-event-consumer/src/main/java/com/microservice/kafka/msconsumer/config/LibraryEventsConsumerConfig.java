710bf9c65fb429025e3b99688046aedf548e2439
Patel <2082383@cognizant.com>
2022-11-11 15:34:35 +0530

    Adding kafka consumer error handler

--- ms-event-consumer/src/main/java/com/microservice/kafka/msconsumer/config/LibraryEventsConsumerConfig.java
+++ ms-event-consumer/src/main/java/com/microservice/kafka/msconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,76 @@
 package com.microservice.kafka.msconsumer.config;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.List;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
+
+    @Autowired
+    KafkaProperties kafkaProperties;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
+        configurer.configure(factory, kafkaConsumerFactory
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
+
+        //To set ack mode manual by default it is batch : LibraryEventsConsumerManualOffset will be used in case of manual ack
+        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        factory.setConcurrency(3);
+        factory.setCommonErrorHandler(errorHandler());
+        return factory;
+    }
+
+    public DefaultErrorHandler errorHandler() {
+
+        var fixedBackOff = new FixedBackOff(1000L, 2L);
+
+        /**
+         * Just the Custom Error Handler
+         */
+         var defaultErrorHandler =  new DefaultErrorHandler(fixedBackOff);
+
+        /**
+         * Error Handler with the BackOff, Exceptions to Ignore, RetryListener
+         */
+
+        /*var defaultErrorHandler = new DefaultErrorHandler(
+                //consumerRecordRecoverer
+                publishingRecoverer()
+                ,
+                fixedBackOff
+                //expBackOff
+        );*/
+
+        //exceptiopnToIgnorelist.forEach(defaultErrorHandler::addNotRetryableExceptions);
+
+        defaultErrorHandler.setRetryListeners(
+                (record, ex, deliveryAttempt) ->
+                        log.info("Failed Record in Retry Listener  exception : {} , deliveryAttempt : {} ", ex.getMessage(), deliveryAttempt)
+        );
+
+        return defaultErrorHandler;
+    }
 }
